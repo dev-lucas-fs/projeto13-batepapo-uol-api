654654500988bb1cd35f856b8c6cef5b2c003086
Lucas F. Silva <lucasdotfs@gmail.com>
2022-11-12 21:40:46 -0300

    feat: adding removeInactiveUsers function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const connection = require("./database");
+const dayjs = require("dayjs");
 
 const app = express();
 
@@ -14,3 +16,23 @@ dotenv.config();
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port ${process.env.PORT}`);
 });
+
+async function removeInactiveUsers() {
+  try {
+    const db = await connection();
+
+    let participants = await db.collection("participants").find().toArray();
+    participants = participants.filter(
+      (participant) =>
+        dayjs(Date.now()).diff(dayjs(participant.lastStatus), "second") > 10
+    );
+
+    participants.forEach(async (participant) => {
+      await db.collection("participants").deleteOne({ name: participant.name });
+    });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+setInterval(removeInactiveUsers, 15 * 1000);
